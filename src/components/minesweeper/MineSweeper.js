e13f3a0edf45781dc9ca2b6ad62bda61b40b22b8
Nicolas Dehes <nicolas.dehes@gmail.com>
2022-04-08 14:31:17 +0200

    Finish minesweeper !

--- src/components/minesweeper/MineSweeper.js
+++ src/components/minesweeper/MineSweeper.js
@@ -11,9 +11,9 @@ export default class MineSweeper extends Component {
       start: false,
       gameOver: false,
       victory: false,
-      longueur: 0,
-      largeur: 0,
-      nbMines: 0,
+      longueur: 8,
+      largeur: 8,
+      nbMines: 10,
       nbFlags: 0,
       gameBoard: [],
       openSetting: false,
@@ -22,12 +22,12 @@ export default class MineSweeper extends Component {
     }
   }
 
-  initBoard = async (x,y) => {
+  initBoard = async (x, y) => {
     //Creation des tableaux vides
     let tempBoard = [];
     let nbMine = this.state.nbMines;
-    if(nbMine > this.state.longueur*this.state.largeur){
-      nbMine = (this.state.longueur*this.state.largeur)-10;
+    if (nbMine > this.state.longueur * this.state.largeur) {
+      nbMine = (this.state.longueur * this.state.largeur) - 10;
     }
     for (let i = 0; i < this.state.largeur; i++) {
       let row = []
@@ -39,8 +39,8 @@ export default class MineSweeper extends Component {
     //Creation des mines
     let positionMine = [];
     for (let m = 0; m < nbMine; m++) {
-      let pos = this.randomisePosition(this.state.largeur, this.state.longueur, x, y)
-      while (positionMine.find(p => p.x === pos.x && p.y === pos.y) !== undefined) {
+      let pos = this.randomisePosition(this.state.largeur, this.state.longueur, x, y) // eslint-disable-next-line
+      while (positionMine.find(p => p.x === pos.x && p.y === pos.y) !== undefined) { 
         pos = this.randomisePosition(this.state.largeur, this.state.longueur, x, y)
       }
       positionMine.push(pos);
@@ -57,41 +57,43 @@ export default class MineSweeper extends Component {
       }
     });
     //Creation des valeurs des cases
-    const nbCases = this.state.longueur*this.state.largeur;
+    const nbCases = this.state.longueur * this.state.largeur;
     this.setState({
       start: true,
-      gameOver: false,
       gameBoard: tempBoard,
       timer: 0,
-      nbFlags:0,
+      nbFlags: 0,
       nbCases: nbCases
-    },()=>{
-      this.clickCase(x,y)
-      const timerInterval = setInterval(()=>{
-        this.setState({timer : this.state.timer +1 })
-        if(this.state.gameOver || this.state.victory){
+    }, () => {
+      this.clickCase(x, y)
+      const timerInterval = setInterval(() => {
+        this.setState({ timer: this.state.timer + 1 })
+        if (this.state.gameOver || this.state.victory || !this.state.start) {
           clearInterval(timerInterval);
         }
-      },1000)
+        if (!this.state.start) {
+          this.setState({ timer: 0 })
+        }
+      }, 1000)
     });
   }
 
   randomisePosition = (x, y, xStart, yStart) => {
-      let newX = Math.floor(Math.random() * x);
-      let newY = Math.floor(Math.random() * y);
-      while(this.plusOuMoinsUn(newX,xStart) && this.plusOuMoinsUn(newY,yStart)){
-        newX = Math.floor(Math.random() * x);
-        newY = Math.floor(Math.random() * y);
-      }
-      return({
-        x: newX,
-        y: newY
-      })
+    let newX = Math.floor(Math.random() * x);
+    let newY = Math.floor(Math.random() * y);
+    while (this.plusOuMoinsUn(newX, xStart) && this.plusOuMoinsUn(newY, yStart)) {
+      newX = Math.floor(Math.random() * x);
+      newY = Math.floor(Math.random() * y);
+    }
+    return ({
+      x: newX,
+      y: newY
+    })
   }
 
-  plusOuMoinsUn = (a,b) => {
-    let c = a-b;
-    return (c<2 && c>-2);
+  plusOuMoinsUn = (a, b) => {
+    let c = a - b;
+    return (c < 2 && c > -2);
   }
 
   flagCase = (x, y) => event => {
@@ -101,13 +103,13 @@ export default class MineSweeper extends Component {
       if (!tempBoard[x][y].isShow) {
         tempBoard[x][y].isFlag = !tempBoard[x][y].isFlag;
         let addToNbFlags = this.state.nbFlags
-        if(tempBoard[x][y].isFlag){
+        if (tempBoard[x][y].isFlag) {
           addToNbFlags++
-        }else{
+        } else {
           addToNbFlags--
         }
-        this.setState({ 
-          gameBoard: tempBoard ,
+        this.setState({
+          gameBoard: tempBoard,
           nbFlags: addToNbFlags
         })
       }
@@ -115,7 +117,7 @@ export default class MineSweeper extends Component {
   }
 
   clickCase = (x, y) => {
-    if(this.state.start){
+    if (this.state.start) {
       if (!this.state.gameOver && !this.state.victory) {
         let tempBoard = this.state.gameBoard;
         if (!tempBoard[x][y].isShow && !tempBoard[x][y].isFlag) {
@@ -123,7 +125,7 @@ export default class MineSweeper extends Component {
           if (tempBoard[x][y].value === 0) {
             for (let i = x - 1; i < x + 2; i++) {
               for (let j = y - 1; j < y + 2; j++) {
-                if (tempBoard[i] != undefined && tempBoard[i][j] !== undefined && !tempBoard[i][j].isShow) {
+                if (tempBoard[i] !== undefined && tempBoard[i][j] !== undefined && !tempBoard[i][j].isShow) {
                   this.clickCase(i, j);
                 }
               }
@@ -135,16 +137,16 @@ export default class MineSweeper extends Component {
             this.gameOver();
           } else {
             let nbCase = 0;
-            tempBoard.map(row => {
-              row.map(c => {
-                if(!c.isShow) nbCase++;
+            tempBoard.forEach(row => {
+              row.forEach(c => {
+                if (!c.isShow) nbCase++;
               })
             })
-            if(nbCase === this.state.nbMines){
-              this.setState({victory: true})
+            if (nbCase === this.state.nbMines) {
+              this.setState({ victory: true })
             }
           }
-        } else if(!tempBoard[x][y].isFlag){
+        } else if (!tempBoard[x][y].isFlag) {
           let numFlagArround = 0;
           for (let i = x - 1; i < x + 2; i++) {
             for (let j = y - 1; j < y + 2; j++) {
@@ -153,10 +155,10 @@ export default class MineSweeper extends Component {
               }
             }
           }
-          if (numFlagArround == tempBoard[x][y].value) {
+          if (numFlagArround === tempBoard[x][y].value) {
             for (let i = x - 1; i < x + 2; i++) {
               for (let j = y - 1; j < y + 2; j++) {
-                if (tempBoard[i] != undefined && tempBoard[i][j] !== undefined && !tempBoard[i][j].isShow && !tempBoard[i][j].isFlag) {
+                if (tempBoard[i] !== undefined && tempBoard[i][j] !== undefined && !tempBoard[i][j].isShow && !tempBoard[i][j].isFlag) {
                   this.clickCase(i, j);
                 }
               }
@@ -164,8 +166,8 @@ export default class MineSweeper extends Component {
           }
         }
       }
-    }else{
-      this.initBoard(x,y);
+    } else {
+      this.initBoard(x, y);
     }
   }
 
@@ -174,9 +176,9 @@ export default class MineSweeper extends Component {
     tempBoard.map(row => {
       return (
         row.map(c => {
-          if(c.isBomb && !c.isFlag && c.explose === undefined){
+          if (c.isBomb && !c.isFlag && c.explose === undefined) {
             c.isShow = true
-          } else if(c.isFlag && !c.isBomb){
+          } else if (c.isFlag && !c.isBomb) {
             c.wrongFlag = true
           }
           return c;
@@ -189,41 +191,47 @@ export default class MineSweeper extends Component {
 
   initGame = () => {
     let tempBoard = []
-    for(let i = 0; i < this.state.largeur; i++){
+    for (let i = 0; i < this.state.largeur; i++) {
       let row = []
-      for(let j = 0; j<this.state.longueur; j ++){
-        row.push({x: i, y: j});
+      for (let j = 0; j < this.state.longueur; j++) {
+        row.push({ x: i, y: j });
       }
       tempBoard.push(row);
     }
     this.setState({
       victory: false,
       start: false,
-      gameBoard: tempBoard
+      gameBoard: tempBoard,
+      gameOver: false,
+      openSetting: false,
+      openInfoGame: true,
+      nbFlags: 0,
     })
   }
 
   onChangeInput = (type) => event => {
-    switch(type){
+    switch (type) {
       case "longueur":
-        this.setState({longueur : event.target.value})
+        this.setState({ longueur: event.target.value })
         break;
       case "largeur":
-        this.setState({largeur : event.target.value})
+        this.setState({ largeur: event.target.value })
         break;
       case "mine":
-        if(event.target.value > (this.state.longueur*this.state.largeur)-10){
-          this.setState({nbMines : (this.state.longueur*this.state.largeur)-10})
+        if (event.target.value > (this.state.longueur * this.state.largeur) - 10) {
+          this.setState({ nbMines: (this.state.longueur * this.state.largeur) - 10 })
         } else {
-          this.setState({nbMines : event.target.value})
+          this.setState({ nbMines: event.target.value })
         }
+        break;
+      default:
         break;
     }
   }
   launchLevel = (type) => event => {
-    this.setState({openLevel: false})
-    switch(type){
-      case"easy":
+    this.setState({ openLevel: false })
+    switch (type) {
+      case "easy":
         this.setState({
           longueur: 8,
           largeur: 8,
@@ -231,7 +239,7 @@ export default class MineSweeper extends Component {
           openInfoGame: true
         })
         break;
-      case"normal":
+      case "normal":
         this.setState({
           longueur: 16,
           largeur: 16,
@@ -239,7 +247,7 @@ export default class MineSweeper extends Component {
           openInfoGame: true
         })
         break;
-      case"hard":
+      case "hard":
         this.setState({
           longueur: 30,
           largeur: 16,
@@ -247,11 +255,15 @@ export default class MineSweeper extends Component {
           openInfoGame: true
         })
         break;
-      case"custom":
+      case "custom":
         this.setState({
-          openSetting: true,
-          openInfoGame: true
+          longueur: 10,
+          largeur: 10,
+          nbMines: 10,
+          openSetting: true
         })
+        break;
+      default:
         break;
     }
   }
@@ -269,10 +281,10 @@ export default class MineSweeper extends Component {
   }
 
   render() {
-    const width = (this.state.longueur*30)+"px";
-    const heigth = (this.state.largeur*30)+"px";
+    const width = (this.state.longueur * 34) + "px";
+    const heigth = ((this.state.largeur * 34) + 34) + "px";
     let key = 1;
-    
+
     return (
       <div className="minesweeper">
         <header>
@@ -296,78 +308,68 @@ export default class MineSweeper extends Component {
                 <p>???</p>
               </div>
             </div>
-            <div className={ this.state.openSetting ? "custom-setting" : "displayNone"}>
+            <div className={this.state.openSetting ? "custom-setting" : "displayNone"}>
               <label>
-                Longueur du plateau : <input type="text" value={this.state.longueur} onChange={this.onChangeInput('longueur')}/>
-              </label>
+                Longueur du plateau : <input type="text" value={this.state.longueur} onChange={this.onChangeInput('longueur')} />
+              </label><br />
               <label>
                 Hauteur du plateau : <input type="text" value={this.state.largeur} onChange={this.onChangeInput('largeur')} />
-              </label>
+              </label><br />
               <label>
                 Nombre de mine : <input type="text" value={this.state.nbMines} onChange={this.onChangeInput('mine')} />
-              </label>
+              </label><br />
+              <button className="button" onClick={this.initGame}>Start</button>
             </div>
           </div>
           <div className={this.state.openInfoGame ? "info-game" : "displayNone"}>
             <div>
-              <img className="info-icon" src="flag.png"/>
+              <img className="info-icon" src="flag.png" alt="flag" />
               {this.state.nbFlags} / {this.state.nbMines}
             </div>
             <div>
-              <button onClick={this.initGame}>Start</button>
+              <button className="button" onClick={this.initGame}>Start</button>
             </div>
             <div>
-              <img className="info-icon" src="chrono.png"/>
-              {Math.floor(this.state.timer/60)} : {("0" + this.state.timer%60).slice(-2)}
+              <img className="info-icon" src="chrono.png" alt="chrono" />
+              {Math.floor(this.state.timer / 60)} : {("0" + this.state.timer % 60).slice(-2)}
             </div>
             <div>
-              <button onClick={this.changeLevel}>Changer la difficulté</button>
+              <button className="button" onClick={this.changeLevel}>Changer la difficulté</button>
             </div>
           </div>
         </header>
         <div className="victory">
-              {this.state.victory?
-                "VICTOIRE !"
-              : this.state.gameOver ?
-                "PERDU !"
+          {this.state.victory ?
+            "VICTOIRE !"
+            : this.state.gameOver ?
+              "PERDU !"
               : ""}
-            </div>
-        <div className="mine-board" style={{width: width, height: heigth}}>
+        </div>
+        <div className="mine-board" style={{ width: width, height: heigth }}>
           {this.state.gameBoard !== undefined ?
             this.state.gameBoard.map(row => {
               return (
                 <div className="mine-row" key={key++}>
                   {row.map(c => {
-                    if (c.explose != undefined && c.explose) {
-                      return (
-                        <div className="mine-case-show-bomb" key={key++}><img className="gameIcon" src="bomb.png" /></div>
-                      )
-                    } else if(c.wrongFlag != undefined && c.wrongFlag){
-                      return (
-                        <div className="mine-case-show-bomb" key={key++}><img className="gameIcon" src="flag.png" /></div>
-                      )
-                    } else if(c.isShow && c.isBomb){
-                      return (
-                        <div className="mine-case-hide" key={key++}><img className="gameIcon" src="bomb.png" /></div>
-                      )
-                    } else if (c.isShow && c.value == 0) {
-                      return (
-                        <div className="mine-case-show" key={key++}></div>
-                      )
-                    } else if (c.isShow) {
-                      const cssClass = "mine-case-show case-" + c.value;
-                      return (
-                        <div className={cssClass} key={key++} onClick={() => this.clickCase(c.x, c.y)}>{c.value}</div>
-                      )
-                    } else if (c.isFlag) {
-                      return (
-                        <div className="mine-case-hide" key={key++} onContextMenu={this.flagCase(c.x, c.y)} onClick={() => this.clickCase(c.x, c.y)}><img className="gameIcon" src="flag.png" /></div>
-                      )
-                    } else {
-                      return (
-                        <div className="mine-case-hide" key={key++} onContextMenu={this.flagCase(c.x, c.y)} onClick={() => this.clickCase(c.x, c.y)}></div>
-                      )
-                    }
+                    return (
+                      <div className="mine-case" style={{ width: this.state.longueur / width, height: this.state.largeur / heigth }}>
+                        {(c.explose !== undefined && c.explose) ?
+                          <div className="mine-case-show-bomb" key={key++}><img className="gameIcon" src="bomb.png" alt="bomb" /></div>
+                          : (c.wrongFlag !== undefined && c.wrongFlag) ?
+                            <div className="mine-case-show-bomb" key={key++}><img className="gameIcon" src="flag.png" alt="flag" /></div>
+                            : (c.isShow && c.isBomb) ?
+                              <div className="mine-case-hide" key={key++}><img className="gameIcon" src="bomb.png" alt="bomb" /></div>
+                              : (c.isShow && c.value === 0) ?
+                                <div className="mine-case-show" key={key++}></div>
+                                : (c.isShow) ?
+                                  <div className={"mine-case-show case-" + c.value} key={key++} onClick={() => this.clickCase(c.x, c.y)}>{c.value}</div>
+                                  : (c.isFlag) ?
+                                    <div className="mine-case-hide" key={key++} onContextMenu={this.flagCase(c.x, c.y)} onClick={() => this.clickCase(c.x, c.y)}><img className="gameIcon" src="flag.png" alt="flag" /></div>
+                                    :
+                                    <div className="mine-case-hide" key={key++} onContextMenu={this.flagCase(c.x, c.y)} onClick={() => this.clickCase(c.x, c.y)}></div>
+                        }
+                      </div>
+                    )
                   })}
                 </div>
               )
