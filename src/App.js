37ca7741b04e13240c5904c93225a3a701affb62
Darzonix <darzonix974@gmail.com>
2022-04-08 10:40:19 +0200

    commit remplacement mine par cliker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React, { Component } from 'react'
-import MinesWeeper from './components/minesweeper/Index';
+import Clicker from './components/clicker/Index';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <MinesWeeper/>
+        <Clicker/>
       </div>
     )
   }
---
+++ src/components/clicker/Index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Clicker extends Component {
+
+  render() {
+    return (
+        <div>
+            <p>Salut</p>
+        </div>
+    )
+  }
+}
